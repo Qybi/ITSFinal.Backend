3625ee886745dad9e9a6dd2d0b12891981bb9942
Raffaele <raffaele.trivillin@gmail.com>
2025-07-14 16:00:42 +0200

    function sutff

--- src/TrivillinRaffaele.Dequeuer/Program.cs
+++ src/TrivillinRaffaele.Dequeuer/Program.cs
@@ -1,7 +1,11 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TrivillinRaffaele.DataAccess.Abstractions.Contexts;
+using TrivillinRaffaele.DataAccess.Contexts;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
@@ -10,6 +14,11 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights();
+
+builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("azuresql") ?? throw new InvalidOperationException("Connection string 'azuresql' not found."));
+});
 
 builder.Services.AddUnitOfWork();
 
