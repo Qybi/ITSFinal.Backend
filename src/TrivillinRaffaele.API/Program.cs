f097df5dedbb7046bbe4cc4a4bceeb4316e0a502
Raffaele <raffaele.trivillin@gmail.com>
2025-07-14 13:04:43 +0200

    di: add di services

--- src/TrivillinRaffaele.API/Program.cs
+++ src/TrivillinRaffaele.API/Program.cs
@@ -1,17 +1,47 @@
+using TrivillinRaffaele.DataAccess.Abstractions.Contexts;
+using TrivillinRaffaele.DataAccess.Contexts;
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("azuresql") ?? throw new InvalidOperationException("Connection string 'azuresql' not found."));
+});
+
+builder.Services.AddUnitOfWork();
+
 builder.Services.AddOpenApi();
+builder.Services.AddCors(c =>
+{
+    c.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+// ignore possible reference loops in json serialization
+builder.Services.Configure<JsonOptions>(options =>
+    options.SerializerOptions.ReferenceHandler =
+        System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/openapi/v1.json", "API V1");
+    });
 }
 
 if (app.Environment.IsProduction())
-{
     app.UseHttpsRedirection();
-}
+
+
 
 app.Run();
